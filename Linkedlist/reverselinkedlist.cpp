86d6e729b348109bfc24fe4d1f9cb45d5c0d5455
chethan40 <chethang1672@gmail.com>
2025-02-27 18:46:28 +0530

    Create reverse2dlinkedlist.cpp

---
+++ Linkedlist/reverselinkedlist.cpp
@@ -0,0 +1,48 @@
+//Reverse a linked list
+
+//input : 3->4->5
+//output : 5->4->3
+
+//brute force:
+//use stack push into it first then pop
+
+DLLNode* reverseDLL(DLLNode* head) {
+        if(head==nullptr || head->next==nullptr) return head;
+  
+        //push
+        DLLNode* temp=head;
+        stack<int> st;
+        while(temp!=nullptr)
+        {
+            st.push(temp->data);
+            temp=temp->next;
+        }
+        //pop
+        temp=head;
+        while(temp!=nullptr)
+        {
+            temp->data=st.top();
+            st.pop();
+            temp=temp->next;
+        }
+        return head;
+    }
+
+//Optimal
+//reverse next and prev pointer of a node
+
+DLLNode* reverseDLL(DLLNode* head) {
+        if(head==nullptr || head->next==nullptr) return head;
+        
+        DLLNode* current=head;
+        DLLNode* last=nullptr;
+        while(current!=nullptr)
+        {
+            last=current->prev;             //temp=a
+            current->prev=current->next;    //a=b
+            current->next=last;             //b=temp
+            
+            current=current->prev;          //to move to next node
+        }
+        return last->prev;
+    }
